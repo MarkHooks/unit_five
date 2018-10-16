e0ecb570125e09a986a064b79f32a718e1b9273f
Mark <mark@iMac2.local>
2018-10-16 12:06:30 -0400

    got the program to get if the user wants to play and the correct random number

--- assignment_five.py
+++ assignment_five.py
@@ -1,25 +1,37 @@
+#this program is to create a guessing game where the user must guess the computers number.
 import random
 
+
 def random_number():
-    print(random.randint(1, 100))
+    number = random.randint(1, 100)
+    print(number)
+    return number
+
 
 def answer():
-
     user_answer = input("guess a number between 1 and 100")
     return float(user_answer)
 
 
 def main():
-    random_number1 = random_number()
+    number = random_number()
     while True:
-        answer(user_answer)
-        if answer(user_answer) == random_number1:
-            print("congratulations, you got the number")
+        play = input("would you like to guess my number?(y/n)")
+        if play == "n":
+            pass
+        elif play == "y":
             break
         else:
-            pass
-
-
+            return False
+    while True:
+        guess = int(input("guess a number between 1 and 100"))
+        if guess == number:
+            print("congratulations, you got my number, which was", number)
+            break
+        elif number < guess:
+            print("your guess is too high")
+        else:
+            print("your guess is too low")
 
 
 main()
